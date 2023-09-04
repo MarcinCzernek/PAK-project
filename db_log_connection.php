0b45a3f310f6cdeb8cd25395ed9d772eed624666
Marcin Czernek <63116220+MarcinCzernek@users.noreply.github.com>
2023-09-04 10:52:38 +0200

    Update db_log_connection.php

--- db_log_connection.php
+++ db_log_connection.php
@@ -5,7 +5,7 @@ $db_user = "root";
 $db_password = "";
 $db_name = "pak_login";
 
-$con = mysqli_connect('localhost',"root","","pak_login");
+$con = mysqli_connect('sql8.freemysqlhosting.net',"sql8644297","YQuz12rqnt","sql8644297");
 
 //Sprawdza połączenie
 if(mysqli_connect_errno()){
